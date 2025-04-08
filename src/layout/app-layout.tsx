a8ead35d597f27f9ec37956bf2091e1f24f40342
Andrey Savka <a_sawka@yahoo.com>
2025-04-08 05:30:44 +0000

    fix content padding

--- src/layout/app-layout.tsx
+++ src/layout/app-layout.tsx
@@ -1,9 +1,8 @@
-
 import { FC, useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { Nav } from "react-bootstrap";
 import AppHeader from "./app-header";
-import './app-layout.scss';
+import "./app-layout.scss";
 
 const AppLayout: FC = () => {
   const [isPanelOpen, setIsPanelOpen] = useState(true);
@@ -15,18 +14,24 @@ const AppLayout: FC = () => {
   return (
     <div className="app-wrapper">
       <AppHeader toggleSidePanel={toggleSidePanel} />
-      <div className={`side-panel ${!isPanelOpen ? 'closed' : ''}`}>
+      <div className={`side-panel ${!isPanelOpen ? "closed" : ""}`}>
         <Nav className="flex-column">
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
-          <Nav.Link as={Link} to="/add-expense">Add Expense</Nav.Link>
-          <Nav.Link as={Link} to="/logout">Logout</Nav.Link>
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/add-expense">
+            Add Expense
+          </Nav.Link>
+          <Nav.Link as={Link} to="/logout">
+            Logout
+          </Nav.Link>
         </Nav>
       </div>
-      <div className={`container ${!isPanelOpen ? 'panel-closed' : ''}`}>
+      <div className={`main-content ${!isPanelOpen ? "panel-closed" : ""}`}>
         <Outlet />
       </div>
     </div>
   );
-}
+};
 
 export default AppLayout;
